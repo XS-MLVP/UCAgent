3b10ef261a135a49b6a8821dabd80e656eb9fd6f
yaozhicheng <yaozhicheng@ict.ac.cn>
2025-06-12 18:34:16 +0800

    fix stream mode exception

--- vagent/verify_agent.py
+++ vagent/verify_agent.py
@@ -1,8 +1,9 @@
 #coding=utf-8
 
 from .util.config import get_config
-from .util.log import info
+from .util.log import info, message, warning, error
 from .util.functions import fmt_time_deta, get_template_path, render_template_dir, append_time_str
+from .util.functions import fill_dlist_none, dump_as_json, get_ai_message_tool_call
 
 from .tools.fileops import *
 from .tools.human import *
@@ -12,6 +13,7 @@ import time
 import random
 import signal
 import openai
+import copy
 
 from langchain.globals import set_debug
 from langchain_openai import ChatOpenAI
@@ -19,28 +21,27 @@ from langchain_core.messages.utils import count_tokens_approximately
 from langchain_core.callbacks import BaseCallbackHandler
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, ToolMessage
 from langmem.short_term import SummarizationNode
 from langgraph.prebuilt.chat_agent_executor import AgentState
 from typing import Any, Dict
+
+class SummarizationAndIgnoreNoneNode(SummarizationNode):
+    def _func(self, input: dict[str, Any] | BaseModel) -> dict[str, Any]:
+        for msg in input["messages"][-4:]:
+            if not isinstance(msg, AIMessage):
+                continue
+            if hasattr(msg, "additional_kwargs"):
+                msg.additional_kwargs = fill_dlist_none(msg.additional_kwargs, '{}', "arguments")
+            if hasattr(msg, "invalid_tool_calls"):
+                msg.invalid_tool_calls = fill_dlist_none(msg.invalid_tool_calls, '{}', "args")
+        return super()._func(input)
+
 
 class State(AgentState):
     # NOTE: we're adding this key to keep track of previous summary information
     # to make sure we're not summarizing on every LLM call
     context: dict[str, Any] 
-
-
-class CbPrintLLMNewToken(BaseCallbackHandler):
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        if self.vagent._new_ai_message:
-            self.vagent._new_ai_message = False
-            print("\n\n ================================== AI Message  =================================", flush=True)
-        print(token, end="", flush=True)
-
-    def bindv(self, vagent):
-        """Bind the callback handler to the agent."""
-        self.vagent = vagent
-        return self
 
 
 class VerifyAgent(object):
@@ -75,8 +76,6 @@ class VerifyAgent(object):
             self.model = ChatOpenAI(openai_api_key=self.cfg.openai.openai_api_key,
                                     openai_api_base=self.cfg.openai.openai_api_base,
                                     model=self.cfg.openai.model_name,
-                                    streaming=True if stream_output else False,
-                                    callbacks=[CbPrintLLMNewToken().bindv(self)] if stream_output else None,
                                     )
         self.workspace = os.path.abspath(workspace)
         template = get_template_path(self.cfg.template, template_dir)
@@ -100,9 +99,9 @@ class VerifyAgent(object):
                            # test:
                            # ...
                            # HumanHelp(),
-                           ] + self.stage_manager.new_tools()
+                           ] + self.stage_manager.new_tools() + (ex_tools if ex_tools is not None else [])
 
-        summarization_node = SummarizationNode( 
+        summarization_node = SummarizationAndIgnoreNoneNode(
             token_counter=count_tokens_approximately,
             model=self.model,
             max_tokens=40000,
@@ -112,15 +111,15 @@ class VerifyAgent(object):
 
         self.agent = create_react_agent(
             model=self.model,
-            tools=self.test_tools + (ex_tools if ex_tools is not None else []),
+            tools=self.test_tools,
             checkpointer=MemorySaver(),
-            pre_model_hook=summarization_node, 
+            pre_model_hook=summarization_node,
             state_schema=State,
         )
         # flags
-        self._new_ai_message = True
         self.stream_output = stream_output
         self.invoke_round = 0
+        self._tool__call_error = []
         self._is_exit = False
         self._tip_index = 0
         self._need_human_input = False
@@ -178,6 +177,8 @@ class VerifyAgent(object):
         return {"messages": [msg]}
 
     def get_current_tips(self):
+        if self._tool__call_error:
+            return {"messages": copy.deepcopy(self._tool__call_error)}
         if self._need_human_input:
             return self.get_human_input()
         messages = self.stage_manager.get_current_tips()
@@ -201,36 +202,87 @@ class VerifyAgent(object):
         info("Verify Agent started at: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_start)))
         while not self.is_exit():
             tips = self.get_current_tips()
-            info("Tips: " + str(tips))
             if self.is_exit():
                 break
-            for i in range(3):
-                try:
-                    self.do_work(tips, self.get_work_config())
-                    break
-                except openai.BadRequestError as e:
-                    warning(f"tips: {tips}")
-                    warning(f"OpenAI API BadRequestError: {e}, try again in 3 seconds...")
-                    time.sleep(3)
-                    tips = self.get_current_tips()
-                assert i < 2, "OpenAI API BadRequestError, please check your input or configuration"
+            self.do_work(tips, self.get_work_config())
         time_end = time.time()
         info("Verify Agent finished at: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_end)))
         info(f"Total time taken: {fmt_time_deta(time_end - time_start)}")
 
     def do_work(self, instructions, config):
         """Perform the work using the agent."""
+        self._tool__call_error = []
+        if self.stream_output:
+            self.do_work_stream(instructions, config)
+        else:
+            self.do_work_values(instructions, config)
+
+    def do_work_values(self, instructions, config):
         last_msg_index = None
         for _, step in self.agent.stream(instructions, config, stream_mode=["values"]):
             index = len(step["messages"])
             if index == last_msg_index:
                 continue
             last_msg_index = index
-            if isinstance(step["messages"][-1], AIMessage) and self.stream_output:
-                continue
-            self._new_ai_message = True
-            print(step["messages"][-1].pretty_repr(), flush=True)
+            msg = step["messages"][-1]
+            self.check_tool_call_error(msg)
+            message(msg.pretty_repr())
             if self._need_human_input:
                 self._evnt_human_input = False
-                print("\n\n[Human Input ]:", end="", flush=True)
+                message("\n\n[Human Input ]:", end="")
                 break
+
+    def do_work_stream(self, instructions, config):
+        last_msg_index = None
+        fist_ai_message = True
+        for v, data in self.agent.stream(instructions, config, stream_mode=["values", "messages"]):
+            if v == "messages":
+                if fist_ai_message:
+                    fist_ai_message = False
+                    message("\n\n================================== AI Message ==================================")
+                msg = data[0]
+                message(msg.content, end="")
+                if self._need_human_input:
+                    self._evnt_human_input = False
+                    message("\n\n[Human Input ]:", end="")
+                    break
+            else:
+                index = len(data["messages"])
+                if index == last_msg_index:
+                    continue
+                last_msg_index = index
+                msg = data["messages"][-1]
+                if isinstance(msg, AIMessage):
+                    message(get_ai_message_tool_call(msg))
+                    self.check_tool_call_error(msg)
+                    continue
+                message(msg.pretty_repr())
+
+    def check_tool_call_error(self, msg):
+        if not isinstance(msg, AIMessage):
+            return
+        if not hasattr(msg, "invalid_tool_calls"):
+            return
+        if len(msg.invalid_tool_calls) < 1:
+            return
+        for call in msg.invalid_tool_calls:
+            name = call.get("name")
+            tool = next((tool for tool in self.test_tools if tool.name == name), None)
+            args = call.get("args") or {}
+            status = "success"
+            try:
+                assert tool is not None, f"Tool {name} not found"
+                result = tool._run(*(), **args)
+            except Exception as e:
+               error(f"Error executing tool {call}: {e}")
+               result = str(e)
+               status = "error"
+            if not isinstance(result, str):
+                result = dump_as_json(result)
+            self._tool__call_error.append(ToolMessage(
+                content=result,
+                tool_call_id=call["id"],
+                name=name,
+                status=status
+            ))
+        warning(f"Tool call error: {msg.invalid_tool_calls}, have re-called them in custom way")
