fd5df041a5e9b5c2934b6f47f87046f687b6da90
yaozhicheng <yaozhicheng@ict.ac.cn>
2025-06-13 17:16:22 +0800

    sync

--- vagent/verify_pdb.py
+++ vagent/verify_pdb.py
@@ -2,7 +2,8 @@
 
 from pdb import Pdb
 import os
-from vagent.util.log import echo_g, echo_y, echo_r
+from vagent.util.log import echo_g, echo_y, echo_r, echo
+from vagent.util.functions import dump_as_json
 
 
 class VerifyPDB(Pdb):
@@ -38,6 +39,31 @@ class VerifyPDB(Pdb):
         ret = [os.path.join(path, f) for f in os.listdir(path) if f.startswith(fname)]
         ret = [f + ("/" if os.path.isdir(os.path.join(path, f)) else "") for f in ret]
         return [f[wk_size + 1:] for f in ret]
+
+    def api_parse_args(self, arg):
+        """Parse arguments for the command
+
+        Args:
+            arg (string): Arguments string, eg: a,b,c,key1=value1,key2=value2
+
+        Returns:
+            tuple: (args, kwargs)
+        """
+        arg = arg.strip()
+        if not arg:
+            return (), {}
+        k = {}
+        a = []
+        for v in arg.split(","):
+            v = v.strip()
+            if "=" in v:
+                key, value = v.split("=", 1)
+                key = key.strip()
+                value = value.strip()
+                k[key] = eval(value)
+            else:
+                a.append(eval(v.strip()))
+        return tuple(a), k
 
     def do_ls(self, arg):
         """
@@ -105,3 +131,92 @@ class VerifyPDB(Pdb):
             return
         return self.do_next_round("")
     do_nrm = do_next_round_with_message
+
+    def do_tool_list(self, arg):
+        """
+        Display tools info.
+        Args:
+            name (str): Name of the tool to display info for. fault is Empty, list all available tools.
+        """
+        tool_name = arg.strip()
+        if not tool_name:
+            tnames = [tool.name for tool in self.agent.test_tools]
+            echo_g(f"Available tools ({len(tnames)}):\n{', '.join(tnames)}")
+            return
+        tool = [tool for tool in self.agent.test_tools if tool.name == tool_name]
+        if not tool:
+            echo_y(f"Tool '{tool_name}' not found.")
+            return
+        tool = tool[0]
+        echo_g(f"[Name]: {tool.name}")
+        echo_g(f"[Description]:\n{tool.description}")
+        if tool.args:
+            echo_g(f"[Args]:\n{dump_as_json(tool.args)}")
+
+    def complete_tool_list(self, text, line, begidx, endidx):
+        """
+        Auto-complete the tool_list command.
+        """
+        if not text:
+            return [tool.name for tool in self.agent.test_tools]
+        return [tool.name for tool in self.agent.test_tools if tool.name.startswith(text.strip())]
+
+    def do_tool_invoke(self, arg):
+        """
+        Invoke a tool with the specified arguments.
+        Args:
+            name (str): Name of the tool to invoke.
+            args (str): Arguments to pass to the tool
+        """
+        args = arg.strip().split()
+        if not args:
+            echo("Usage: tool_invoke <tool_name> [arg1,arg2,arg3,key1=value1,key2=value2, ...]")
+            return
+        tool_name = args[0]
+        tool = [tool for tool in self.agent.test_tools if tool.name == tool_name]
+        if not tool:
+            echo_y(f"Tool '{tool_name}' not found.")
+            return
+        tool = tool[0]
+        input_a = " ".join(args[1:])
+        try:
+            a, k = self.api_parse_args(input_a)
+            k = tool.tool_call_schema.model_construct(a, **k)  # Validate arguments against the tool's schema
+        except Exception as e:
+            echo_r(f"Error parsing arguments: {e}")
+            return
+        try:
+            echo(dump_as_json(tool._run(**k.model_dump())))
+        except Exception as e:
+            echo_r(f"Error invoking tool '{tool_name}': {e}")
+            return
+
+    def complete_tool_invoke(self, text, line, begidx, endidx):
+        """
+        Auto-complete the tool_invoke command.
+        """
+        return self.complete_tool_list(text, line, begidx, endidx)
+
+    def do_status(self, arg):
+        """
+        Display the current status of the agent.
+        """
+        pass
+
+    def do_task_list(self, arg):
+        """
+        List all tasks in the current workspace.
+        """
+        pass
+
+    def do_get_sys_tips(self, arg):
+        """
+        Get system tips.
+        """
+        pass
+
+    def do_set_sys_tips(self, arg):
+        """
+        Set system tips.
+        """
+        pass
