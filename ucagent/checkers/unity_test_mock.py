050815d800d58f871af74c3b7d2f0589e02a81b9
yaozhicheng <yaozhicheng@ict.ac.cn>
2026-01-22 16:11:56 +0800

    draft: test mock in batch

---
+++ ucagent/checkers/unity_test_mock.py
@@ -0,0 +1,117 @@
+#coding: utf-8
+
+import os
+from ucagent.tools.testops import RunUnityChipTest
+import ucagent.util.functions as fc
+from ucagent.checkers.base import Checker, UnityChipBatchTask
+from typing import Tuple
+from ucagent.util.log import info
+
+
+class UnityChipCheckerTestMockTestBatch(Checker):
+
+    def __init__(self, target_file, test_file_prefix, test_prefix, test_dir="tests",
+                 first_arg="",
+                 last_arg="",
+                 batch_size=1, 
+                 min_file_tests=1, timeout=300, **kw):
+        self.target_file = target_file
+        self.test_file_prefix = test_file_prefix
+        self.test_prefix = test_prefix
+        self.test_dir = test_dir
+        self.first_arg = first_arg
+        self.last_arg = last_arg
+        self.batch_size = batch_size
+        self.min_file_tests = min_file_tests
+        self.timeout = timeout
+        self.run_test = RunUnityChipTest()
+        self.batch_task = UnityChipBatchTask("mock_test_file", self)
+
+    def set_workspace(self, workspace: str):
+        """
+        Set the workspace for the test case checker.
+
+        :param workspace: The workspace directory to be set.
+        """
+        super().set_workspace(workspace)
+        self.run_test.set_workspace(workspace)
+        return self
+
+    def on_init(self):
+        self.batch_task.source_task_list = fc.find_files_by_pattern(self.workspace, self.target_file)
+        self.batch_task.update_current_tbd()
+        info(f"Load mock_component list(size={len(self.batch_task.source_task_list)}).")
+        return super().on_init()
+
+    def do_check(self, timeout, is_complete=False,  **kw) -> Tuple[bool, object]:
+        """Check the ptest test implementation for correctness."""
+        test_dir_full_path = self.get_path(self.test_dir)
+        if not os.path.exists(test_dir_full_path):
+            return False, {"error": f"test directory '{self.test_dir}' does not exist in workspace."}
+        test_files = fc.find_files_by_pattern(self.workspace, self.target_file_list)
+        
+
+
+    def do_one_check(self, test_files, test_dir_full_path, timeout=0) -> Tuple[bool, object]:
+        if len(test_files) == 0:
+            tfiles = ', '.join(self.target_file_list)
+            return False, {"error": f"No test files found with pattern '{tfiles}' in workspace."}
+        error_cases = []
+        for tfile in test_files:
+            if test_dir_full_path not in self.get_path(tfile):
+                error_cases.append(f"The test file '{tfile}' is not under the test directory '{self.test_dir}'.")
+                continue
+            test_func_list = fc.get_target_from_file(self.get_path(tfile), f"test*",
+                                                         ex_python_path=self.workspace,
+                                                         dtype="FUNC")
+            for test_func in test_func_list:
+                if test_func.__name__.startswith(self.test_prefix) is False:
+                    error_cases.append(f"The '{test_func.__name__}' test function's name must start with '{self.test_prefix}'.")
+                    continue
+                args = fc.get_func_arg_list(test_func)
+                if self.first_arg and (len(args) < 1 or args[0] != self.first_arg):
+                    error_cases.append(f"The '{test_func.__name__}' test function's first arg must be '{self.first_arg}', but got ({', '.join(args)}).")
+                if self.last_arg and (len(args) < 1 or args[-1] != self.last_arg):
+                    error_cases.append(f"The '{test_func.__name__}' test function's last arg must be '{self.last_arg}', but got ({', '.join(args)}).")
+            if len(test_func_list) < self.min_file_tests:
+                error_cases.append(f"Insufficient testcases: {len(test_func_list)} test functions found, minimum required is {self.min_file_tests} in file '{tfile}'. "+
+                                    "Please ensure you have implemented enough test cases (need pytest function based not class based).")
+        if len(error_cases) > 0:
+            return False, {
+                "error": "Check test functions failed.",
+                "details": error_cases
+            }
+        # run test
+        timeout = timeout if timeout > 0 else self.timeout
+        self.run_test.set_pre_call_back(
+            lambda p: self.set_check_process(p, timeout + 10)  # Set the process for the checker
+        )
+        py_case_files = [fc.rm_workspace_prefix(test_dir_full_path,
+                                                self.get_path(t)) for t in test_files]
+        report, str_out, str_err = self.run_test.do(
+            test_dir_full_path,
+            pytest_ex_args=" ".join(py_case_files),
+            return_stdout=True, return_stderr=True, return_all_checks=True,
+            timeout=timeout
+        )
+        test_pass, test_msg = fc.is_run_report_pass(report, str_out, str_err)
+        if not test_pass:
+            return False, test_msg
+        if not report or "tests" not in report:
+            return False, {
+                "error": f"Test execution failed or returned invalid report.",
+                "STD_OUT": str_out,
+                "STD_ERR": str_err,
+            }
+        tc_total = report["tests"]["total"]
+        tc_failed = report["tests"]["fails"]
+        if tc_failed > 0:
+            return False, {
+                "error": f"Test failed: {tc_failed}/{tc_total} test cases failed. Need all test cases to pass.",
+                "STD_OUT": str_out,
+                "STD_ERR": str_err,
+            }
+        ret, msg = fc.check_has_assert_in_tc(self.workspace, report)
+        if not ret:
+            return ret, msg
+        return True, {"message": f"{self.__class__.__name__} check passed."}
